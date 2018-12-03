9f37f7a727d727d9e6a0d600dbd15081ad31a029
study20171001 <3032979812@qq.com>
2018-12-03 10:32:55 +0800

    [chore] add styled-components

--- pages/demo/index.tsx
+++ pages/demo/index.tsx
@@ -1,15 +1,29 @@
+import styled from 'styled-components'
 import routes from '../routes'
 const { Link } = routes
 
+const DemoStyled = styled.div`
+  padding: 10px;
+
+  h1{
+    color: blue;
+    font-size: 30px;
+  }
+  li{
+    font-size: 20px;
+  }
+`
+
 const Demo = () => {
   return (
-    <div>
+    <DemoStyled>
       <h1>this is test 'demo' page!</h1>
       <ul>
         <li>jump <Link route="/demo/nextRoutes"><a>/demo/nextRoutes</a></Link> page</li>
         <li>jump <Link route="/demo/koaFetch"><a>/demo/koaFetch</a></Link> page</li>
       </ul>
-    </div>
+      <div>goBack <Link route="/"><a>/index</a></Link> page</div>
+    </DemoStyled>
   )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,27 @@
+import styled from 'styled-components'
 import routes from './routes'
 const { Link } = routes
 
+const Container = styled.div`
+  padding: 10px;
+
+  h1{
+    color: green;
+    font-size: 30px;
+  }
+  li{
+    font-size: 20px;
+  }
+`
+
 const Home = () => {
   return (
-    <div>
+    <Container>
       <h1>Welcome to next.js!</h1>
       <ul>
         <li>jump <Link route="/demo"><a>/demo</a></Link> page</li>
       </ul>
-    </div>
+    </Container>
   )
 }
 
