6987850987691c5b24095777c6b133cc1b2a21ec
study20171001 <3032979812@qq.com>
2018-12-03 15:13:37 +0800

    [chore] add fastclick.js 解决移动端（ipad）点击事件反应慢问题（300毫秒延迟）

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -27,6 +27,13 @@ export default class MyDocument extends Document<Props> {
           <Main />
           <NextScript />
 
+          {/* fastclick.js 解决移动端（ipad）点击事件反应慢问题（300毫秒延迟） */}
+          <script dangerouslySetInnerHTML={{
+            __html: `if ('addEventListener' in document) {
+              document.addEventListener('DOMContentLoaded', () => {
+                FastClick.attach(document.body)
+              }, false)
+            }` }} />
           {/* Eruda 是一个专为手机网页前端设计的调试面板，类似 DevTools 的迷你版，其主要功能包括：
             捕获 console 日志、检查元素状态、显示性能指标、捕获 XHR 请求、显示本地存储和 Cookie 信息、浏览器特性检测等 */}
           {process.env.DEPLOY_ENV !== 'production' && [
